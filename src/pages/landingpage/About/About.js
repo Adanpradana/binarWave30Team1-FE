a93761d07f91f3cdf4637e6ba9e7d1d574dad74d
labibplop <lbabss9@gmail.com>
2023-05-22 17:49:30 +0700

    about us. di package ada tambahan buat iconnya

--- src/pages/landingpage/About/About.js
+++ src/pages/landingpage/About/About.js
@@ -1,9 +1,257 @@
-import { Button } from "react-bootstrap";
-
+import React from "react";
+import member1 from "../../../assets/member1.png";
+import member2 from "../../../assets/member2.png";
+import member3 from "../../../assets/member3.png";
+import member4 from "../../../assets/member4.png";
+import member5 from "../../../assets/member5.png";
+import member6 from "../../../assets/member6.png";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "./About.css";
 export default function About() {
   return (
-    <div>
-      <Button color="primary">hello im about</Button>
-    </div>
+    <section className="section-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h2 className="section-title">The Team Behind Good Game</h2>
+            <p className="section-subtitle">
+              Dalam kamus bahasa indonesia “game” diartikan permainan. Permainan
+              adalah bagian dari bermain dan bermain juga bagian dari permainan,
+              keduanya ini saling berhubungan. Permaian merupakan kegiatan
+              kompleks yang didalamnya terdapat peraturan, play dan budaya.
+              Sebuah permainan ialah sebuah sistem dimana pemain terlibat dalam
+              konflik buatan, disini pemain bernteraksi dengan sistem konflik
+              dalam permaianan ialah rekayasa atau buatan, dalam permainan
+              terdapat peraturan yang bertujuan untuk membatasi perilakuk pemain
+              dan juga menentukan permainan.
+            </p>
+          </div>
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="col-sm-6 col md-4">
+            <div className="team-item">
+              <img src={member1} className="team-img" alt="pic" />
+              <h3>LABIB PLOP</h3>
+              <div className="team-info">
+                <p>PENGANGGURAN</p>
+                <p>
+                  pengangguran adalah seorang yang tidak bekerja yang selalu
+                  main game mulu
+                </p>
+
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="LinkedIn">
+                      <i>
+                        <LinkedInIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Github">
+                      <i>
+                        <GithubIcon />
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="Instagram">
+                      <i>
+                        <InstagramIcon />
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
